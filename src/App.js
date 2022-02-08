859e8534a2524490de736bb27efd2da3fcad75ef
ShreyaDhir <dhirshreya@gmail.com>
2022-02-08 11:39:46 +0530

    update dark mode and footer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import Routes from './components/Routes';
+import Navigator from './components/Navigator';
 
 const App = () => {
     const [ darkTheme, setDarkTheme ] = useState(false);
   return (
         <div className={darkTheme ? 'dark' : ''}>
             <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
-                <Navbar />
-                <Routes />
+                <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme}/>
+                <Navigator />
                 <Footer />
             </div>
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Navbar = () => {
+import Search from './Search';
+
+const Navbar = ({darkTheme, setDarkTheme}) => {
   return (
         <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark-border-gray-700 border-gray-200">
             <div className="flex justify-between items-center space-x-5 w-screen">
                 <Link to="/">
-                    <p className="text-2xl">
-
+                    <p className="text-2xl bg-blue-500 font-bold text-white py-1 px-2 rounded dark:bg-gray-500 dark:text-gray-900">
+                        Forage 🔎
                     </p>
                 </Link>
+                <button className="dark:bg-gray-500 dark:bg-text-900 bg-white border rounded-full px-2 py-1 hover:shadow-lg" type="button" onClick={() => setDarkTheme(!darkTheme)}>
+                    {darkTheme ? '☀️': '🌙'}
+                </button>
             </div>
+            <Search />
         </div>
     );
 };
